f0a9ee9eeed31410aecb7bc0172cec615698aecd
Saso Kiselkov <saso.kiselkov@nexenta.com>
2014-10-21 22:41:26 +0200

    NEX-3514 Implement persistent L2ARC Reviewed by: Alek Pinchuk <alek.pinchuk@nexenta.com> Reviewed by: Josef Sipek <josef.sipek@nexenta.com>

--- usr/src/uts/common/sys/fs/zfs.h
+++ usr/src/uts/common/sys/fs/zfs.h
@@ -23,6 +23,7 @@
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2011, 2014 by Delphix. All rights reserved.
  * Copyright (c) 2013, Joyent, Inc. All rights reserved.
+ * Copyright (c) 2013, Saso Kiselkov. All rights reserved.
  * Copyright 2015 Nexenta Systems, Inc.  All rights reserved.
  */
 
@@ -629,6 +630,7 @@ typedef struct zpool_rewind_policy {
 #define	ZPOOL_CONFIG_IS_LOG		"is_log"
 #define	ZPOOL_CONFIG_IS_SPECIAL		"is_special"
 #define	ZPOOL_CONFIG_L2CACHE		"l2cache"
+#define	ZPOOL_CONFIG_L2CACHE_PERSISTENT	"l2cache_persistent"
 #define	ZPOOL_CONFIG_HOLE_ARRAY		"hole_array"
 #define	ZPOOL_CONFIG_VDEV_CHILDREN	"vdev_children"
 #define	ZPOOL_CONFIG_IS_HOLE		"is_hole"
