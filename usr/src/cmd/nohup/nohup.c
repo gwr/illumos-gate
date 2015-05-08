008ccd3246e64383912678df355e71d5702be0d6
Gordon Ross <gwr@nexenta.com>
2015-05-08 10:07:46 -0400

    5876 intel sys/regset.h pollutes name space (nohup)

--- usr/src/cmd/nohup/nohup.c
+++ usr/src/cmd/nohup/nohup.c
@@ -27,7 +27,7 @@
 /*	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T	*/
 /*	  All Rights Reserved	*/
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
+#define	_REGSET_NAMES	/* see <sys/regset.h> */
 
 #include <stdio.h>
 #include <stdlib.h>
