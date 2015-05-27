f0140e87540f4f91774b5a8f2a2eb5be835b78d2
Rick McNeal <rick.mcneal@nexenta.com>
2015-05-26 21:29:00 -0600

    NEX-3746 Panic seen if iSCSI Initiator sends a SCSI Response packet to target NEX-3777 COMSTAR iscsi/io test can panic the target running 5.0 NEX-3785 This is on 5.0 release and iSCSI target can panic while running iSCSI mpxio disable test Reviewed by: Rob Gittins <rob.gittins@nexenta.com>

--- usr/src/uts/common/io/idm/idm_conn_sm.c
+++ usr/src/uts/common/io/idm/idm_conn_sm.c
@@ -493,6 +493,7 @@ idm_login_timeout(void *arg)
 {
 	idm_conn_t *ic = arg;
 
+	ic->ic_state_timeout = 0;
 	idm_conn_event(ic, CE_LOGIN_TIMEOUT, NULL);
 }
 
@@ -829,6 +830,7 @@ idm_logout_req_timeout(void *arg)
 {
 	idm_conn_t *ic = arg;
 
+	ic->ic_state_timeout = 0;
 	idm_conn_event(ic, CE_LOGOUT_TIMEOUT, NULL);
 }
 
@@ -909,6 +911,7 @@ idm_cleanup_timeout(void *arg)
 {
 	idm_conn_t *ic = arg;
 
+	ic->ic_state_timeout = 0;
 	idm_conn_event(ic, CE_CLEANUP_TIMEOUT, NULL);
 }
 
