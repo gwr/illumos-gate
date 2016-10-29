a59e8126eb38734e95f67a60eccac726e823207f
Gordon Ross <gordon.w.ross@gmail.com>
2016-10-29 12:32:42 -0400

    Temporary hack: pre-build gfx_private for x86, used when building gfx-drm (not planning to upstream this commit)

---
+++ make-gfxp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+[ "$SRC" ] || { echo "Must set SRC=... first!" ; exit 1; }
+
+(cd usr/src/uts/common/sys && make install_h)
+(cd usr/src/uts/i86pc/gfx_private && make install)
+
+gfxp_files="usr/include/sys/gfx_private.h
+platform/i86pc/kernel/misc/amd64/gfx_private
+platform/i86pc/kernel/misc/gfx_private"
+
+(cd proto/root_i386 && tar cf ../../gfxp.tar $gfxp_files)
