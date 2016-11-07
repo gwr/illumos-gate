99a84554818842827ba76e43d8000958ffa9da36
Gordon Ross <gwr@nexenta.com>
2016-11-07 11:35:34 -0500

    NEX-8852 Quality-of-Service (QoS) controls per NFS share
    1st Experimental implementation of QoS in ZFS

--- usr/src/uts/common/fs/zfs/sys/zfs_vfsops.h
+++ usr/src/uts/common/fs/zfs/sys/zfs_vfsops.h
@@ -42,6 +42,20 @@ typedef struct zfsvfs zfsvfs_t;
 struct znode;
 
 /*
+ * ZFS Quality of Service (QoS) I/O throttling state,
+ * per file system.  Limits the I/O rate in this FS.
+ * See "Token Bucket" on Wikipedia
+ */
+typedef struct zfs_qos_state {
+	uint64_t qos_rate_cap;		/* zero means no cap */
+	int64_t qos_token_bucket;	/* bytes I/O allowed without waiting */
+	hrtime_t qos_last_update;
+	kmutex_t qos_lock;
+	kcondvar_t qos_wait_cv;
+	int qos_waiters;
+} zfs_qos_state_t;
+
+/*
  * Status of the zfs_unlinked_drain thread.
  */
 typedef enum drain_state {
@@ -100,6 +114,7 @@ struct zfsvfs {
 	kmutex_t	z_drain_lock;
 	kcondvar_t	z_drain_cv;
 	drain_state_t	z_drain_state;
+	zfs_qos_state_t z_qos;
 };
 
 /*
