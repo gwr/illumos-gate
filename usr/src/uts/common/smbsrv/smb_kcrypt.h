d8b1019f21c2f8e5f7e265611e2b72c78ca2c23d
Alexander Stetsenko <alex.stetsenko@gmail.com>
2022-01-24 14:41:30 +0300

    BSR-8441 SMB should allow 256 bit ciphers
    import 14047 Add support for SMB 3.1.1 AES-256
    from code.illumos.org/c/illumos-gate/+/1980
    (with some rework)

--- usr/src/uts/common/smbsrv/smb_kcrypt.h
+++ usr/src/uts/common/smbsrv/smb_kcrypt.h
@@ -11,7 +11,7 @@
 
 /*
  * Copyright 2017 Nexenta Systems, Inc.  All rights reserved.
- * Copyright 2020 RackTop Systems, Inc.
+ * Copyright 2022 RackTop Systems, Inc.
  */
 
 #ifndef _SMB_KCRYPT_H_
@@ -40,7 +40,6 @@ extern "C" {
 #define	SHA512_DIGEST_LENGTH	64	/* SHA512 digest length in bytes */
 #define	SMB2_SIG_SIZE		16
 #define	SMB2_KEYLEN		16
-#define	SMB3_KEYLEN		16	/* AES-128 keys */
 
 #ifdef	_KERNEL
 /* KCF variant */
@@ -95,12 +94,16 @@ int smb2_hmac_init(smb_sign_ctx_t *, smb_crypto_mech_t *, uint8_t *, size_t);
 int smb2_hmac_update(smb_sign_ctx_t, uint8_t *, size_t);
 int smb2_hmac_final(smb_sign_ctx_t, uint8_t *);
 
+int smb2_hmac_one(smb_crypto_mech_t *mech, uint8_t *key, size_t key_len,
+    uint8_t *data, size_t data_len, uint8_t *mac, size_t mac_len);
+
 int smb3_cmac_getmech(smb_crypto_mech_t *);
 int smb3_cmac_init(smb_sign_ctx_t *, smb_crypto_mech_t *, uint8_t *, size_t);
 int smb3_cmac_update(smb_sign_ctx_t, uint8_t *, size_t);
 int smb3_cmac_final(smb_sign_ctx_t, uint8_t *);
 
-int smb3_kdf(uint8_t *outbuf, uint8_t *key, size_t key_len,
+int smb3_kdf(uint8_t *outbuf, uint32_t outbuf_len,
+    uint8_t *key, size_t key_len,
     uint8_t *label, size_t label_len,
     uint8_t *context, size_t context_len);
 
