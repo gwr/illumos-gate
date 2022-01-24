c924ae3eb5082d9e2a3ed6b2cfeeba90622225f9
Vitaliy Gusev <gusev.vitaliy@gmail.com>
2022-01-24 12:20:23 +0300

    BSR-10970 pynfs/nfs4.1: OP_CLOSE should return special invalid stateid

--- usr/src/uts/common/fs/nfs/nfs4_srv.c
+++ usr/src/uts/common/fs/nfs/nfs4_srv.c
@@ -8559,6 +8559,7 @@ out:
 	    SETCLIENTID_CONFIRM4 *, res);
 }
 
+extern stateid4 invalid_stateid;
 
 /*ARGSUSED*/
 void
@@ -8651,8 +8652,6 @@ rfs4_op_close(nfs_argop4 *argop, nfs_resop4 *resop,
 
 	/* Update the stateid. */
 	next_stateid(&sp->rs_stateid);
-	resp->open_stateid = sp->rs_stateid.stateid;
-
 	rfs4_dbe_unlock(sp->rs_dbe);
 
 	rfs4_update_lease(sp->rs_owner->ro_client);
@@ -8661,6 +8660,8 @@ rfs4_op_close(nfs_argop4 *argop, nfs_resop4 *resop,
 
 	rfs4_state_close(sp, FALSE, FALSE, cs->cr);
 
+	/* See RFC8881 section 18.2.4, and RFC7530 section 16.2.5 */
+	resp->open_stateid = invalid_stateid;
 	*cs->statusp = resp->status = status;
 
 end:
