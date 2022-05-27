c10dddc0ffe4d1913bd1467757668a1a28edf810
Gordon Ross <gwr@racktopsystems.com>
2022-05-27 10:29:33 -0400

    BSR-11318 smbd becomes unresponsive on memory allocation failures

--- usr/src/cmd/smbsrv/smbd/smbd_authsvc.c
+++ usr/src/cmd/smbsrv/smbd/smbd_authsvc.c
@@ -11,6 +11,7 @@
 
 /*
  * Copyright 2017 Nexenta Systems, Inc.  All rights reserved.
+ * Copyright 2022 RackTop Systems, Inc.
  */
 
 /*
@@ -276,6 +277,7 @@ smbd_authsvc_listen(void *arg)
 			smbd_authsvc_thrcnt--;
 			(void) mutex_unlock(&smbd_authsvc_mutex);
 			(void) close(ns);
+			smbd_nomem();
 			continue;
 		}
 		ctx->ctx_socket = ns;
@@ -287,8 +289,12 @@ smbd_authsvc_listen(void *arg)
 			smbd_authsvc_thrcnt--;
 			(void) mutex_unlock(&smbd_authsvc_mutex);
 			smbd_authctx_destroy(ctx);
+			ctx = NULL;
+			smbd_nomem();
+			continue;
 		}
 		ctx = NULL; /* given to the new thread or destroyed */
+		(void) pthread_detach(tid);
 	}
 
 out:
@@ -517,6 +523,9 @@ smbd_authsvc_dispatch(authsvc_context_t *ctx)
 	default:
 		return (-1);
 	}
+
+	if (rc == NT_STATUS_NO_MEMORY)
+		smbd_nomem();
 
 	if (rc != 0) {
 		smb_lsa_eresp_t *er = ctx->ctx_orawbuf;
@@ -901,7 +910,7 @@ smbd_authsvc_gettoken(authsvc_context_t *ctx)
 	if (len > ctx->ctx_orawlen) {
 		if ((ctx->ctx_orawbuf = realloc(ctx->ctx_orawbuf, len)) ==
 		    NULL) {
-			return (NT_STATUS_INTERNAL_ERROR);
+			return (NT_STATUS_NO_MEMORY);
 		}
 	}
 
